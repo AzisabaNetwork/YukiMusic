776f568d25a2e8baa4dcb1624ca12e5867a87720
YukiLeafX <yukileafx@gmail.com>
2020-06-21 19:16:00 +0900

    /play-nbs-url <URL> コマンドを追加

--- src/main/kotlin/io/github/yukileafx/yukimusic/YukiMusic.kt
+++ src/main/kotlin/io/github/yukileafx/yukimusic/YukiMusic.kt
@@ -1,9 +1,11 @@
 package io.github.yukileafx.yukimusic
 
+import io.github.yukileafx.yukimusic.command.PlayNBSURLCommand
 import org.bukkit.plugin.java.JavaPlugin
 
 class YukiMusic : JavaPlugin() {
 
     override fun onEnable() {
+        getCommand("play-nbs-url").executor = PlayNBSURLCommand(this)
     }
 }
---
+++ src/main/kotlin/io/github/yukileafx/yukimusic/command/PlayNBSURLCommand.kt
@@ -0,0 +1,41 @@
+package io.github.yukileafx.yukimusic.command
+
+import io.github.yukileafx.yukimusic.YukiMusic
+import io.github.yukileafx.yukimusic.nbs.NBSInputStream
+import org.bukkit.ChatColor
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import java.net.URL
+
+class PlayNBSURLCommand(private val plugin: YukiMusic) : CommandExecutor {
+
+    override fun onCommand(
+        sender: CommandSender,
+        command: Command,
+        label: String,
+        args: Array<out String>
+    ): Boolean {
+        if (args.isEmpty()) {
+            return false
+        }
+        plugin.server.scheduler.runTaskAsynchronously(plugin) {
+            try {
+                val urlSpec = args.joinToString(" ")
+                sender.sendMessage("Connecting to $urlSpec ...")
+
+                val url = URL(urlSpec)
+                val urlConn = url.openConnection()
+                urlConn.setRequestProperty("User-Agent", "Mozilla/5.0")
+                val inputStream = urlConn.getInputStream()
+                val nbsIn = NBSInputStream(inputStream)
+
+                val header = nbsIn.readHeader()
+                sender.sendMessage(header.toString())
+            } catch (t: Throwable) {
+                sender.sendMessage("${ChatColor.RED}Failed to connect URL.")
+            }
+        }
+        return true
+    }
+}
